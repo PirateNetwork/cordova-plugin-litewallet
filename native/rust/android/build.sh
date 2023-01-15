d35b2724fb88cfd6443cc7f84f49abb03f61a72d
Cryptoforge <cryptoforge.cc@gmail.com>
2023-01-15 00:50:04 -0800

    add x86_64 android build target

--- native/rust/android/build.sh
+++ native/rust/android/build.sh
@@ -20,6 +20,12 @@ export CC=$NDK/x86/bin/i686-linux-android-clang
 export OPENSSL_DIR=$(pwd)/openssl-1.1.1e/openssl-1.1.1e/x86
 cargo build --target i686-linux-android --release
 
+export AR=$NDK/x86_64/bin/x86_64-linux-android-ar
+export CC=$NDK/x86_64/bin/x86_64-linux-android-clang
+export OPENSSL_DIR=$(pwd)/openssl-1.1.1e/openssl-1.1.1e/x86
+cargo build --target x86_64-linux-android --release
+
 cp ../target/aarch64-linux-android/release/librust.so ../../android/litewallet-jni/libs/arm64-v8a/liblitewallet-jni.so
 cp ../target/armv7-linux-androideabi/release/librust.so ../../android/litewallet-jni/libs/armeabi-v7a/liblitewallet-jni.so
 cp ../target/i686-linux-android/release/librust.so ../../android/litewallet-jni/libs/x86/liblitewallet-jni.so
+cp ../target/x86_64-linux-android/release/librust.so ../../android/litewallet-jni/libs/x86_64/liblitewallet-jni.so
